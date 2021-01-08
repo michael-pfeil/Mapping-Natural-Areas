3e05b4b2b7d099af3c18eaa6cc605eb361dbad6b
Jenny Pfeil <jennifer.pfeil@mail.utoronto.ca>
2021-01-08 13:08:26 -0700

    Original version

---
+++ config.js
@@ -0,0 +1,41 @@
+
+const config = {
+    style: "mapbox://styles/mapbox/light-v10",
+    accessToken: "pk.eyJ1IjoiamVubmlmZXJwZmVpbCIsImEiOiJja2puNzZpdW0wcjN6MnluMDY0MWQyM21pIn0.qfZ6ocRVbznPiGbB-omH3Q",
+    CSV: "https://jennypfeil.github.io/Mapping-Natural-Areas/naturalareasdata.csv",
+    center: [-79.39775965337452,43.663461999999996], //Lng, Lat
+    zoom: 11, //Default zoom
+    title: "Natural Areas Database",
+    description: "You can search by address to sort the list below by distance. You can also filter the park usage, environment and historical significance.",
+    sideBarInfo: ["Name", "Size (ha)"],
+    popupInfo: ["Name"],
+    filters: [
+        {
+            type: "checkbox",
+            title: "Usage: ",
+            columnHeader: "Usage",
+            listItems: ["Walking", "Biking", "Tennis Courts", "Ice Rink", "Sports Fields", "Dog Park", "Zoo", "Farm", "Weekend Market"]
+        },
+        {
+            type: "checkbox",
+            title: "Environment: ",
+            columnHeader: "Environment",
+            listItems: ["Hill", "Open Fields", "Pond", "Wooded", "Ravine", "Small Urban Farm"]
+        },
+        {
+            type: "dropdown",
+            title: "Title of filter: ",
+            columnHeader: "Column Name",
+            listItems: [
+                'filter one',
+                'filter two',
+                'filter three',
+                'filter four',
+                'filter five',
+                'filter six',
+                'filter seven'
+            ]
+        }
+    ]
+
+};
