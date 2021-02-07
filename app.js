e016863798d1d498ceb4b4373c727f7e372935bb
Michael Pfeil <micha.pfeil@gmail.com>
2021-02-06 17:24:17 -0700

    Changed markers to green.

--- app.js
+++ app.js
@@ -451,7 +451,7 @@ map.on('load', function () {
                     },
                     paint: {
                         'circle-radius': 5, // size of circles
-                        'circle-color': '#3D2E5D', // color of circles
+                        'circle-color': 'green', // color of circles
                         'circle-stroke-color': 'white',
                         'circle-stroke-width': 1,
                         'circle-opacity': 0.7,
